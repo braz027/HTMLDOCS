40150e0a5405bc21db0618b2d3afc4d30d265129
braz027 <62607410+braz027@users.noreply.github.com>
2020-04-16 13:59:39 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+document.onload = function(){
+  if(localStorage.name != null && localStorage.email != null && localStorage.msg != null){
+    document.getElementById('localoutput').innerHTML=
+      "Name: "+localStorage.name+" Email: "+localStorage.email+" Message: "+localStorage.msg+". This information was saved from last time!";
+  }
+  if(sessionStorage.name != null && sessionStorage.email != null && sessionStorage.msg != null){ //should always be false after refresh
+    document.getElementById('sessionoutput').innerHTML=
+      "Name: "+sessionStorage.name+" Email: "+sessionStorage.email+" Message: "+sessionStorage.msg+". This information was saved from last time!";
+  }
+  if(localStorage.name != null && sessionStorage.name == null){
+    document.getElementById('sessionoutput').innerHTML="Nothing to display here :( I didn't save your input.";
+  }
+};
+
+function localclick() {
+  var name = document.getElementById('localname').value;
+  var email=document.getElementById('localmail').value;
+  var msg=document.getElementById('localmsg').value;
+  var output=document.getElementById('localoutput');
+  localStorage.name=name;
+  localStorage.email=email;
+  localStorage.msg=msg;
+  output.innerHTML="Name: "+localStorage.name+" Email: "+localStorage.email+" Message: "+localStorage.msg;
+}
+function sessionclick() {
+  var name = document.getElementById('sessionname').value;
+  var email=document.getElementById('sessionmail').value;
+  var msg=document.getElementById('sessionmsg').value;
+  var output=document.getElementById('sessionoutput');
+  sessionStorage.name=name;
+  sessionStorage.email=email;
+  sessionStorage.msg=msg;
+  output.innerHTML="Name: "+sessionStorage.name+" Email: "+sessionStorage.email+" Message: "+sessionStorage.msg;
+}
